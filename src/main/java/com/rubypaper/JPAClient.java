08f71166c14046f3151b894629326504e43a0ebe
Sejin Kim <lemon05090@naver.com>
2022-06-22 16:53:47 +0900

    [using createQuery() method] select list of the board.title

--- src/main/java/com/rubypaper/JPAClient.java
+++ src/main/java/com/rubypaper/JPAClient.java
@@ -7,6 +7,7 @@ import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
 import java.util.Date;
+import java.util.List;
 
 public class JPAClient {
     public static void main(String[] args){
@@ -23,27 +24,34 @@ public class JPAClient {
             // Transaction begin
             entityTransaction.begin();
 
-            /*Board board = new Board();
+            Board board = new Board();
             board.setTitle("JPA init");
             board.setWriter("Sejin");
             board.setContent("게시판 프로젝트로 넘어갑니다");
             board.setCreateDate(new Date());
-            board.setCnt(0L);*/
+            board.setCnt(0L);
 
-            Board board = entityManager.find(Board.class, 7L);
+            // Board board = entityManager.find(Board.class, 7L);
 
             // 테이블 수정
             // board.setTitle("검색한 게시글의 제목 수정");
 
             // 등록 -> 영속화
             // [DML] INSERT, UPDATE, DELETE는 안 됨 <- Commit을 해줘야 함.(transaction 관리)
-            // entityManager.persist(board);
+            entityManager.persist(board);
 
             // 엔티티 삭제
-            entityManager.remove(board);
+            // entityManager.remove(board);
 
             // Transaction commit
             entityTransaction.commit();
+
+            //글목록 조회
+            String jpql = "select b from Board b order by b.seq desc";
+            List<Board> boardList = entityManager.createQuery(jpql, Board.class).getResultList();
+            for (Board brd : boardList){
+                System.out.println("---> " + brd.toString());
+            }
         } catch (Exception e) {
             e.printStackTrace();
 
